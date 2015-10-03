e82de8b303a986e892247cfa2d37ca055e2e829f
Nguyễn Quý Tú <quytutlu@gmail.com>
2015-10-03 16:05:09 +0700

    up date login fb

--- SmartHome/ViewController.swift
+++ SmartHome/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate
+{
 
     @IBOutlet weak var TenDangNhap: UITextField!
     
@@ -16,10 +19,58 @@ class ViewController: UIViewController {
     var ThamSoTruyen:NSUserDefaults!
     
     @IBAction func DangNhap(sender: AnyObject) {
-        let url="http://smarthometl.com/index.php/?cmd=dangnhap&tendangnhap="+TenDangNhap.text!+"&matkhau="+MatKhau.text!
+        let url="http://localhost/index.php/?cmd=dangnhap&tendangnhap="+TenDangNhap.text!+"&matkhau="+MatKhau.text!
         APIDangNhap(url)
     }
+    override func viewDidLoad() {
+        if(FBSDKAccessToken.currentAccessToken()==nil){
+            print("Not logged in...")
+            FBSDKAccessToken.setCurrentAccessToken(nil)
+        }else{
+            print("Logged in..")
+            FBSDKAccessToken.setCurrentAccessToken(nil)
+        }
+        let loginButton=FBSDKLoginButton()
+        loginButton.readPermissions=["public_profile","email","user_friends"]
+        loginButton.delegate=self
+        loginButton.center=self.view.center;
+        self.view.addSubview(loginButton)
+        
+    }
+    func APIDangNhapFb(postString:String){
+        let request = NSMutableURLRequest(URL: NSURL(string: "http://localhost/index.php")!)
+        request.HTTPMethod = "POST"
+        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil {
+                print("error=\(error)")
+                return
+            }
+            
+            let responseString:NSString = NSString(data: data!, encoding: NSUTF8StringEncoding)!
+            let jsonData:NSData = responseString.dataUsingEncoding(NSUTF8StringEncoding)!
+            let json = JSON(data:jsonData)
+            print(json["active"].stringValue)
+            if(json["active"]=="1"){
+                self.ChuyenManHinh(json["id"].stringValue)
+            }else{
+                self.showMess()
+            }
+        }
+        task.resume()
+    }
+    func ChuyenManHinh(id:String){
+        ThamSoTruyen=NSUserDefaults()
+        ThamSoTruyen.setObject(id, forKey: "idNguoiDung")
+        if let resultController = storyboard!.instantiateViewControllerWithIdentifier("MHTrangThai") as? DsThietBiTableViewController {
+                presentViewController(resultController, animated: true, completion: nil)
+        }
+        
+    }
     func APIDangNhap(urlString:String){
+        print(urlString)
         ThamSoTruyen=NSUserDefaults()
         if let url = NSURL(string: urlString) {
             if let data = try? NSData(contentsOfURL: url, options: []) {
@@ -34,7 +85,44 @@ class ViewController: UIViewController {
                     showMess()
                 }
             }
+        }else{
+            showMess()
         }
+    }
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        if(error==nil){
+            let accessToken = FBSDKAccessToken.currentAccessToken()
+            let req = FBSDKGraphRequest(graphPath: "me", parameters: ["fields":"email,name"], tokenString: accessToken.tokenString, version: nil, HTTPMethod: "GET")
+            req.startWithCompletionHandler({ (connection, result, error : NSError!) -> Void in
+                if(error == nil)
+                {
+                    //print("result \(result)")
+                    let resultdict = result as? NSDictionary
+                    let idvalue = resultdict?["id"] as! String
+                    //print("the id value is \(idvalue)")
+                    let emailvalue = resultdict?["email"] as! String
+                    //print("the email value is \(emailvalue)")
+                    let namevalue = resultdict?["name"] as! String
+                    //print("the name value is \(namevalue)")
+                    let json="{%22name%22:%22"+namevalue+"%22,%22id%22:%22"+idvalue+"%22}";
+                    var url = "tendangnhapfb="+json+"&email="+emailvalue;
+                    url=url.stringByReplacingOccurrencesOfString(" ", withString: "%20")
+                    self.APIDangNhapFb(url)
+                }
+                else
+                {
+                    print("error \(error)")
+                }
+            })
+            print("login complete..")
+            FBSDKAccessToken.setCurrentAccessToken(nil)
+            
+        }else{
+            print(error.localizedDescription)
+        }
+    }
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User logged out...")
     }
     func showMess() {
         let ac = UIAlertController(title: "Thông báo", message: "Đăng nhập thất bại", preferredStyle: .Alert)
@@ -44,9 +132,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
